b0ba955199b2df6004e1641bf7a6cc20ea46efb7
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-20 22:04:06 -0600

    Update estudiante.cs

--- estudiante.cs
+++ estudiante.cs
@@ -2,10 +2,23 @@ using System;
 
 public class Estudiante
 {
-   
+   public string Curso { get; set; }
     public string Nombre { get; set; }
-    public int Edad { get; set; }
-    public string Curso { get; set; }
+    public string Edad { get; set; }
+
+
+    public Estudiante(string curso, string nombre, string edad)
+    {
+        Curso = curso;
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Curso: " + 4);
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad:" + 18 );
 
     
    
@@ -13,8 +26,17 @@ public class Estudiante
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+        Estudiante estudiante1 = new Estudiante("4", "Carlos", "18");
+        Estudiante estudiante2 = new Estudiante("6", "Willy", "20");
+
+        Console.WriteLine("Información del Estudiante 1:");
+      estudiante1.MostrarInformacion();
+
+        Console.WriteLine("Informacion del estudiante 2:");
+      estudiante2.MostrarInformacion();
+        Console.ReadKey();
+      
     }
 }
