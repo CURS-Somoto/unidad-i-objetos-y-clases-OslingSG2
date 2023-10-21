ef5ccd557db84b23b1a5c149417a34dc02f40dac
OslingSG2 <147261945+OslingSG2@users.noreply.github.com>
2023-10-20 21:52:15 -0600

    Update mascota.cs
    tarea editada

--- mascota.cs
+++ mascota.cs
@@ -2,13 +2,39 @@ using System;
 
 public class Mascota
 {
+    public string Tipo { get; set; }
+    public string Nombre { get; set; }
+    public string Edad { get; set; }
+
+
+    public Mascota(string tipo, string nombre, string edad) 
+    {
+        Tipo = tipo;
+        Nombre = nombre;
+        Edad = edad;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine("Tipo: " + Tipo);
+        Console.WriteLine("Nombre: " + Nombre);
+        Console.WriteLine("Edad:" + 4);
+    }
      
 }
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+       Mascota mascota1 = new Mascota("Pitbull", "Dolar", "4");
+        Mascota mascota2 = new Mascota ("Pastor Aleman", "Body", "6");
+
+        Console.WriteLine("Información de la Mascota 1:");
+        mascota1.MostrarInformacion();
+
+        Console.WriteLine("Informacion de la Mascota 1:");
+        mascota2.MostrarInformacion();
+        Console.ReadKey();
     }
 }
